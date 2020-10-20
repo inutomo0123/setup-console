5ab44cb9a23fefd91e2e109d86283c1ad3ba2446
inukai tomoki <inutomo0123@gmail.com>
2020-10-20 10:45:27 +0900

    emacs, tmux バージョンアップ

--- setup.sh
+++ setup.sh
@@ -2,35 +2,47 @@
 
 set -ue
 
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
-WORK_DIR="/tmp/$(date +%Y%m%d%H%M%S)"
+SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
+
+SRC_DIR="${HOME}/local/src/"
+LOCAL_DIR="${HOME}/local/"
+BIN_DIR="${HOME}/local/bin/"
+
+SERIAL="$(date +%Y%m%d%H%M%S)"
+
+TMUX_VERSION="3.1b"
+EMACS_VERSION="27.1"
 
 # tmux
 function setup-tmux(){
     # 依存パッケージ
-    sudo apt install -y build-essential libncurses5-dev libevent-dev
+    sudo apt install -y \
+         build-essential \
+         libncurses5-dev \
+         libevent-dev
 
     # ダウンロード
-    mkdir -p ${WORK_DIR}
-    cd ${WORK_DIR}
-    curl -OL https://github.com/tmux/tmux/releases/download/3.0a/tmux-3.0a.tar.gz
-    tar -zxvf tmux-3.0a.tar.gz
+    mkdir -p "${SRC_DIR}"
+    cd "${SRC_DIR}"
+    curl -OL \
+         "https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz"
+    tar -zxvf "tmux-${TMUX_VERSION}.tar.gz"
 
     # インストール
-    cd tmux-3.0a
-    ./configure --prefix="${HOME}/local"
+    cd "${TMUX_VERSION}"
+    ./configure --prefix="${LOCAL_DIR}"
     make
     make install
 
     # 設定
-    cd ${SCRIPT_DIR}
-    cp -i ./.tmux.conf ${HOME}/
+    cd "${SCRIPT_DIR}"
+    cp -i ./.tmux.conf "${HOME}/"
 
     # PATH
-    cat <<EOF >> ${HOME}/.bashrc
+    cat <<EOF >> "${HOME}/.bashrc"
 
 # tmux
-PATH="\${HOME}/local/bin:\${PATH}"
+PATH="\${BIN_DIR}:\${PATH}"
 EOF
 
 }
@@ -44,16 +56,21 @@ function setup-powerline(){
     # インストール
     pip3 install --user powerline-status
 
+    # インストール先の取得
+    location=$(pip3 show powerline-status \
+                   | grep "Location" \
+                   | grep -o -E "/.*$")
+
     # 設定
-    cat <<EOF >> ${HOME}/.tmux.conf
+    cat <<EOF >> "${HOME}/.tmux.conf"
 
 # powerline
 run-shell "powerline-daemon -q"
-source "${HOME}/.local/lib/python3.6/site-packages/powerline/bindings/tmux/powerline.conf"
+source "${location}/powerline/bindings/tmux/powerline.conf"
 EOF
 
     # PATH
-    cat <<EOF >> ${HOME}/.bashrc
+    cat <<EOF >> "${HOME}/.bashrc"
 
 # powerline
 PATH="\${HOME}/.local/bin:\${PATH}"
@@ -65,17 +82,23 @@ EOF
 function setup-emacs(){
 
     # 依存パッケージ
-    sudo apt install -y build-essential libncurses5-dev libgnutls28-dev pkg-config mailutils
+    sudo apt install -y \
+         build-essential \
+         libncurses5-dev \
+         libgnutls28-dev \
+         pkg-config \
+         mailutils
 
     # ダウンロード
-    mkdir -p ${WORK_DIR}
-    cd ${WORK_DIR}
-    curl -OL http://gnu.mirrors.hoobly.com/emacs/emacs-26.3.tar.gz
-    tar -zxvf emacs-26.3.tar.gz
+    mkdir -p "${SRC_DIR}"
+    cd "${SRC_DIR}"
+    curl -OL \
+         "http://gnu.mirrors.hoobly.com/emacs/emacs-${EMACS_VERSION}.tar.gz"
+    tar -zxvf "emacs-${EMACS_VERSION}.tar.gz"
 
     # インストール
-    cd ./emacs-26.3
-    ./configure --prefix=${HOME}/local --without-x
+    cd "./emacs-${EMACS_VERSION}"
+    ./configure --prefix="${LOCAL_DIR}" --without-x
     make
     make install
 
@@ -84,22 +107,23 @@ function setup-emacs(){
 function config-emacs(){
 
     # ダウンロード
-    mkdir -p ${HOME}/repo.d
-    cd ${HOME}/repo.d
-    git clone https://github.com/inutomo0123/dotfiles.git
+    mkdir -p "${SRC_DIR}"
+    cd "${SCRIPT_DIR}"
+    git clone "https://github.com/inutomo0123/dotfiles.git"
 
-    # 設定
+    # バックアップ
     if [ -d ${HOME}/.emacs.d ]; then
-	mv ${HOME}/.emacs.d ${HOME}/.emacs.d.org
+	    mv "${HOME}/.emacs.d" "${HOME}/.emacs.d.${SERIAL}"
     fi
-    ln -s ${HOME}/repo.d/dotfiles/.emacs.d ${HOME}/
+
+    ln -s "${SRC_DIR}dotfiles/.emacs.d" "${HOME}/.emacs.d"
 }
 
 function setup-font(){
 
     # ダウンロード
-    mkdir -p ${HOME}/repo.d
-    cd ${HOME}/repo.d/
+    mkdir -p "${SRC_DIR}"
+    cd "${SRC_DIR}"
     git clone https://github.com/powerline/fonts.git
 
     # インストール
@@ -107,10 +131,9 @@ function setup-font(){
     ./install.sh
 }
 
-
 function setup-fish(){
 
-    cd ${SCRIPT_DIR}
+    cd "${SCRIPT_DIR}"
 
     # インストール
     sudo apt-add-repository ppa:fish-shell/release-3
@@ -120,11 +143,11 @@ function setup-fish(){
     # FISHER
     curl -L https://get.oh-my.fish | fish
     curl https://git.io/fisher --create-dirs \
-         -sLo ${HOME}/.config/fish/functions/fisher.fish
+         -sLo "${HOME}/.config/fish/functions/fisher.fish"
     fish --command="fisher add oh-my-fish/theme-bobthefish"
 
     # tmux
-    cat <<EOF >> ${HOME}/.tmux.conf
+    cat <<EOF >> "${HOME}/.tmux.conf"
 
 # デフォルトシェル
 set -g default-command /usr/bin/fish
