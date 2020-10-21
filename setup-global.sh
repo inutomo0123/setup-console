4ce2c0526a7adace9d34fbc764e76de0efd8b9e3
inukai tomoki <inutomo0123@gmail.com>
2020-10-21 15:15:48 +0900

    python3対応

--- setup-global.sh
+++ setup-global.sh
@@ -1,36 +1,95 @@
 #!/bin/bash
 
-set -Cuex
+set -Cue
 
-function step1(){
+SRC_DIR="${HOME}/local/src/"
+LOCAL_DIR="${HOME}/local/"
+BIN_DIR="${HOME}/local/bin/"
 
+SERIAL="$(date +%Y%m%d%H%M%S)"
+
+GNUGLOBAL_VERSION="6.6.5"
+
+# Pygments
+function pygments-setup(){
+
+    sudo apt update
+
+    # ctagのインストール
     sudo apt install exuberant-ctags
 
-    pip3 install --user Pygments
+    # pythonのバージョンによる分岐
+    python_version=$(python --version \
+                         | grep -o "[2,3]\.[0-9]\.[0-9]")
+
+    if [ "${python_version:0:1}" = "2" ]; then
+        forpython2
+    else
+        forpython3
+    fi
 }
 
-function step2(){
-    mkdir ${HOME}/local/src
+function forpython3(){
 
-    cd ${HOME}/local/src
-    wget http://tamacom.com/global/global-6.6.4.tar.gz
+    # pipのインストール
+    sudo apt install python3-pip
 
-    tar -zxvf global-6.6.4.tar.gz
+    # Pygmentsのインストール
+    pip3 install --user Pygment
 
-    cd ./global-6.6.4
+}
 
-    ./configure prefix=${HOME}/local
+function forpython2(){
+
+    # pipのインストール
+    sudo apt install python-pip
+
+    # Pygmentsのインストール
+    pip install --user Pygment
+}
+
+# GNU GLOBAL
+function gnuglobal-setup(){
+
+    mkdir -p "${SRC_DIR}"
+
+    cd "${SRC_DIR}"
+    wget "http://tamacom.com/global/global-${GNUGLOBAL_VERSION}.tar.gz"
+
+    tar -zxvf "global-${GNUGLOBAL_VERSION}.tar.gz"
+
+    cd "./global-${GNUGLOBAL_VERSION}"
+
+    ./configure prefix="${LOCAL_DIR}"
     make
     make install
 }
 
-function step3(){
+function gnuglobal-setting(){
 
-    cp -i  ${HOME}/local/share/gtags/gtags.conf ${HOME}/.globalrc
+    # ~/.globalrc
+    if [ -f "${HOME}/.globalrc" ]; then
+       mv "${HOME}/.globalrc" "${HOME}/.globalrc.${SERIAL}"
+    fi
 
-    sed -i 's/^\t:tc=native:/&tc=pygments:/g' ${HOME}/.globalrc
+    cp -i  "${HOME}/local/share/gtags/gtags.conf" "${HOME}/.globalrc"
+
+    sed -i 's/^\t:tc=native:/&tc=pygments:/g' "${HOME}/.globalrc"
+
+    # PATH
+    if [ -f "${HOME}/.bashrc" ] ; then
+       mv "${HOME}/.bashrc" "${HOME}/.bashrc.${SERIAL}"
+    fi
+
+    if [[ ":${PATH}:" == *:"${BIN_DIR}":* ]]; then
+        cat <<EOF >> "${HOME}/.bashrc"
+
+# GNU GLOBAL
+PATH="${BIN_DIR}:\${PATH}"
+EOF
+    fi
 }
 
-step1
-step2
-step3
+pygments-setup
+gnuglobal-setup
+gnuglobal-setting
